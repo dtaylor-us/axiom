b6d61dd184d300fdaafe10e634005c72ec7512ac
dtaylor-us <derektaylor.us@gmail.com>
2026-07-03 16:08:27 -0500

    forge: Add Swagger configuration to ArchonApiApplication class to enable spr...
    feature-branch run d7ee364ef6b2491e, sub-task 1/6
    Task: Add Swagger configuration to ArchonApiApplication class to enable springdoc-openapi

--- archon-api/src/main/java/com/archon/api/ArchonApiApplication.java
+++ archon-api/src/main/java/com/archon/api/ArchonApiApplication.java
@@ -1,9 +1,12 @@
 package com.archon.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@OpenAPIDefinition(info = @Info(title = "Archon API", version = "v1"))
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ArchonApiApplication {
